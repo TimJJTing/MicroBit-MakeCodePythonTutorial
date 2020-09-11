0eb96cc08e8a6b7ace15baf66b0ca84a564c2bb8
TimJJTing <stu87282@gmail.com>
2020-09-11 22:48:02 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,39 @@
+# def on_button_pressed_a():
+#     global number
+#     number = 13
+# input.on_button_pressed(Button.A, on_button_pressed_a)
+
+# def on_button_pressed_b():
+#     global number
+#     number = 12
+# input.on_button_pressed(Button.B, on_button_pressed_b)
+
+# number = 5
+
+# def on_forever():
+#     basic.show_number(number)
+# basic.forever(on_forever)
+
+# def on_button_pressed_b():
+#     number = 12
+#     print(number)
+# input.on_button_pressed(Button.B, on_button_pressed_b)
+
+# print string
+print("hello world")
+
+# assign a string to var and print
+mystring = "hello heck"
+print(mystring)
+
+# change a string of var and print
+mystring = "hello yo"
+print(mystring)
+
+# assign an int to var and print
+mynumber = 5
+print(mynumber)
+
+# change an int of var and print
+mynumber = 10
+print(mynumber)
---
+++ main.ts
@@ -0,0 +1,30 @@
+//  def on_button_pressed_a():
+//      global number
+//      number = 13
+//  input.on_button_pressed(Button.A, on_button_pressed_a)
+//  def on_button_pressed_b():
+//      global number
+//      number = 12
+//  input.on_button_pressed(Button.B, on_button_pressed_b)
+//  number = 5
+//  def on_forever():
+//      basic.show_number(number)
+//  basic.forever(on_forever)
+//  def on_button_pressed_b():
+//      number = 12
+//      print(number)
+//  input.on_button_pressed(Button.B, on_button_pressed_b)
+//  print string
+console.log("hello world")
+//  assign a string to var and print
+let mystring = "hello heck"
+console.log(mystring)
+//  change a string of var and print
+mystring = "hello yo"
+console.log(mystring)
+//  assign an int to var and print
+let mynumber = 5
+console.log(mynumber)
+//  change an int of var and print
+mynumber = 10
+console.log(mynumber)
